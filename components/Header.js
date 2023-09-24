d418301a1368bca580ead9dd056b9e1bec541910
Wesley Huber <wesleybaxterhuber@gmail.com>
2023-09-23 21:43:52 -0400

    updating blog page

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 function Header() {
-    return (
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="/"><Image
-    className={styles.icon} src="/adaptive-icon.png" // Route of the image file
-    style={{margin: '0 auto', textAlign: 'center'}}
-    height={55} // Desired size with correct aspect ratio
-    width={55} // Desired size with correct aspect ratio
-    alt="Trick Book"
-  /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="privacy-policy">Privacy Policy</Nav.Link>
-              <Nav.Link href="questions-support">Support</Nav.Link>
-              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+	return (
+		<Navbar bg="light" expand="lg">
+			<Container>
+				<Navbar.Brand href="/">
+					<Image
+						className={styles.icon}
+						src="/adaptive-icon.png" // Route of the image file
+						style={{ margin: "0 auto", textAlign: "center" }}
+						height={55} // Desired size with correct aspect ratio
+						width={55} // Desired size with correct aspect ratio
+						alt="Trick Book"
+					/>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link href="/">Home</Nav.Link>
+						<Nav.Link href="blog">Blog</Nav.Link>
+						<Nav.Link href="privacy-policy">Privacy Policy</Nav.Link>
+						<Nav.Link href="questions-support">Support</Nav.Link>
+						{/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
                   Another action
@@ -33,11 +37,11 @@ function Header() {
                   Separated link
                 </NavDropdown.Item>
               </NavDropdown> */}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-  
-  export default Header;
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default Header;
---
+++ pages/blog.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import Head from "next/head";
+import Layout from "../components/layout";
+import Image from "next/image";
+import Header from "../components/Header";
+
+export default function Blog() {
+	return (
+		<>
+			<Head>
+				<title>The Trick Book - Blog</title>
+				<link rel="icon" href="/favicon.png" />
+				<meta name="description" content="The Trick Book - Blog" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="robots" content="index, follow" />
+				<link rel="canonical" href="https://thetrickbook.com/" />
+				<meta name="author" content="Wes Huber" />
+				<meta
+					name="keywords"
+					content="Trick, Book, Skateboarding, Snowboarding, Trickbook, TheTrickBook, App"
+				/>
+			</Head>
+			<Header />
+			<Layout>
+				<h1 className="pt-3" style={{ textAlign: "left" }}>
+					Blog
+				</h1>
+				<p>Hi From blog page</p>
+				<Link href="/">
+					{" "}
+					<span class="material-icons">arrow_back</span> Back to home
+				</Link>
+			</Layout>
+		</>
+	);
+}
