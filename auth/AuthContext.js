fdaf82e684107d07f38d68d415616eb0bf0dbe3a
Wesley Huber <wesleybaxterhuber@gmail.com>
2023-10-22 09:23:25 -0400

    added session storage

--- auth/AuthContext.js
+++ auth/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -6,6 +6,32 @@ export function AuthProvider({ children }) {
 	const [loggedIn, setLoggedIn] = useState(false);
 	const [token, setToken] = useState(null); // Initializing token state
 	const [email, setEmail] = useState(null);
+
+	// Run once when the component mounts to load the initial state from sessionStorage
+	useEffect(() => {
+		const initialToken = sessionStorage.getItem("userToken");
+		const initialEmail = sessionStorage.getItem("userEmail");
+		if (initialToken) {
+			setLoggedIn(true);
+			setToken(initialToken);
+			setEmail(initialEmail);
+		}
+	}, []);
+
+	// Update sessionStorage whenever token or email changes
+	useEffect(() => {
+		if (token) {
+			sessionStorage.setItem("userToken", token);
+		} else {
+			sessionStorage.removeItem("userToken");
+		}
+
+		if (email) {
+			sessionStorage.setItem("userEmail", email);
+		} else {
+			sessionStorage.removeItem("userEmail");
+		}
+	}, [token, email]);
 
 	const logIn = (newToken, email) => {
 		setLoggedIn(true);
