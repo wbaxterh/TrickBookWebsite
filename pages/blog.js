41f3ce64df558aa2898d5769eac649d096f474a0
wbaxterh <wesleybaxterhuber@gmail.com>
2024-01-22 14:20:36 -0500

    adding blog post

--- pages/blog.js
+++ pages/blog.js
@@ -11,44 +11,52 @@ export default function Blog({ allPostsData }) {
 		<>
 			<Head>
 				<title>The Trick Book - Blog</title>
-				<link rel="icon" href="/favicon.png" />
-				<meta name="description" content="The Trick Book - Blog" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta name="robots" content="index, follow" />
-				<link rel="canonical" href="https://thetrickbook.com/" />
-				<meta name="author" content="Wes Huber" />
+				<link rel='icon' href='/favicon.png' />
+				<meta name='description' content='The Trick Book - Blog' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<meta name='robots' content='index, follow' />
+				<link rel='canonical' href='https://thetrickbook.com/' />
+				<meta name='author' content='Wes Huber' />
 				<meta
-					name="keywords"
-					content="Trick, Book, Skateboarding, Snowboarding, Trickbook, TheTrickBook, App"
+					name='keywords'
+					content='Trick, Book, Skateboarding, Snowboarding, Trickbook, TheTrickBook, App'
 				/>
 			</Head>
 			<Header />
 			<Layout>
-				<h1 className="pt-3" style={{ textAlign: "left" }}>
+				<h1 className='pt-3' style={{ textAlign: "left" }}>
 					Blog
 				</h1>
-				<div className="row">
+				<div className='row'>
 					{allPostsData.map(({ id, date, title, author, firstImage }) => (
-						<div className="col-md-3 col-sm-4 col-12 mb-4" key={id}>
-							<div className="card">
-								<img
+						<div className='col-md-4 col-sm-4 col-4 mb-4' key={id}>
+							<div className='card'>
+								<div style={{ position: "relative", height: "150px" }}>
+									<Image
+										src={"/" + firstImage}
+										alt={`${title}`}
+										layout='fill'
+										objectFit='cover'
+									/>
+								</div>
+								{/* <img
 									src={firstImage} // Use the firstImage variable here
 									alt={`${title}`}
-									className="card-img-top"
-								/>
-								<div className="card-body">
-									<h5 className="card-title">
+									className='card-img-top'
+								/> */}
+								<div className='card-body'>
+									<h5 className='card-title'>
 										<Link href={`/blog/${id}`}>{title}</Link>
 									</h5>
-									<p className="card-text">{date}</p>
-									<p className="card-text">By {author}</p>
+									<p className='card-text'>{date}</p>
+									<p className='card-text'>By {author}</p>
 								</div>
 							</div>
 						</div>
 					))}
 				</div>
-				<Link href="/">
-					<span className="material-icons align-middle pb-1">arrow_back</span>
+				<Link href='/'>
+					<span className='material-icons align-middle pb-1'>arrow_back</span>
 					Back to home
 				</Link>
 			</Layout>
---
+++ posts/how-to-boardslide.md
@@ -0,0 +1,38 @@
+---
+title: "How to Boardslide"
+author: "Wes Huber"
+date: "2024-01-21"
+---
+
+## How to Boardslide
+
+Boardslide is easily one of the most fun tricks in skateboarding, once you master it. We'll go over the fundamentals of a boardslide in this tutorial. Check out our tutorial on instagram! @@https://www.instagram.com/p/CyjSxD7R-5C/@@ give us a follow for more trick tips
+
+![How to boardslide](how-to-boardslide.png)
+
+## Prerequisites
+
+Before learning how to boardslide, you should already have a few tricks under your belt.
+
+- The Ollie
+- The Frontside 180
+
+## Practice popping onto a feature
+
+It's important with boardslides to start small and work your way up. I recommend finding a small parking curb or similar obstacle to practice ollieing onto the center of you board. If you can pivot off great! If not, no worries, the main goal of this exercise is to get comfortable ollieing onto the center of your board.
+
+## Now practice the slide on a small ledge or rail
+
+Now that you're comfortable ollieing onto a feature, it's time to try it rolling. Roll at a slow, comfortable speed and ollie on to a small rail or ledge. First start with just hitting the end of the rail or ledge. Try changing up your speed, a lot of times more speed is helpful for boardslides.
+
+## How do I land?
+
+You can land either fakie or regular, whichever comes more naturally to you. All you need to do to land the boardslide is give your board a nudge at the end of the rail to push it back to regular or over to fakie. You should have enough time in the air to get the board straight and land on the bolts.
+
+## Where do I look?
+
+When performing the boardslide it's important to focus on the end of the rail as the target of where you are going. Before you ollie on you should be in line to target the end of the rail, you can line yourself up by looking at the end of the rail. When it's time to ollie on you cna focus on your ollie and the rail in front of you, but it's important to maintain focus on your destination, the end of the rail.
+
+## Conclusion
+
+Those are the steps for getting a boardslide, now go out and practice and have fun with it!
