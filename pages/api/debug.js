d5c43022386cbee9d48429ef3cead1437060f2aa
wbaxterh <wesleybaxterhuber@gmail.com>
2024-06-02 23:11:14 -0400

    update debug

--- pages/api/debug.js
+++ pages/api/debug.js
@@ -1,9 +1,8 @@
-// pages/api/debug.js
 export default function handler(req, res) {
 	res.status(200).json({
-		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+		NEXTAUTH_URL: process.env.NEXTAUTH_URL || "NOT SET",
+		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "NOT SET",
+		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "NOT SET",
+		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "NOT SET",
 	});
 }
