24fcd5b42d5af5a978ab55b8892959ee8109a8ba
wbaxterh <wesleybaxterhuber@gmail.com>
2024-07-06 11:58:37 -0400

    adding scope to the auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,6 +12,11 @@ export default NextAuth({
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			authorization: {
+				params: {
+					scope: "openid email profile",
+				},
+			},
 			async profile(profile) {
 				console.log("Google Profile:", profile);
 
